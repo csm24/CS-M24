18fbc6212c2139299a6bfd3933fb9d7a1bfe9c67
Simon Hewitt <simon.d.hewitt@gmail.com>
2015-04-06 12:14:24 +0100

    very minor

---
+++ V1/doxygen/html/search/functions_6.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['sendcommand',['sendCommand',['../classswantech_1_1_stockfish.html#a0b971d0f645065262487fb597347d74f',1,'swantech::Stockfish']]],
+  ['setchessplayer',['setChessplayer',['../classswantech_1_1_chess_engine.html#aa5030a1e441c0868999509e35652636c',1,'swantech::ChessEngine']]],
+  ['startengine',['startEngine',['../classswantech_1_1_stockfish.html#a19ab1ebc8d457f5743443bebe8ddd252',1,'swantech::Stockfish']]],
+  ['stopengine',['stopEngine',['../classswantech_1_1_stockfish.html#af97e469fd321bac056aed5251caa7284',1,'swantech::Stockfish']]]
+];
